b6e9cfc9745d6bbf62bcfc8ec48cfd1ed8b72829
yhwang1129 <2095482609@qq.com>
2025-02-27 14:16:16 +0800

    添加了备注

--- day03/mysite3/mysite3/settings.py
+++ day03/mysite3/mysite3/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )#项目生成后在补充，是一个元组
